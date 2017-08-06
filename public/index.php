f6977b15e56333198ca275bc2357f6ebf1d762d2
Gegham Ayvazyan <gagvirus92@gmail.com>
2017-08-06 13:45:57 +0400

    Determining host name

--- public/index.php
+++ public/index.php
@@ -1,8 +1,10 @@
 <?php
-    require_once('config.php');
-    //OVERRIDE THE DEFAULT AUTOLOAD TO PREVENT REPEATED
-    //DIRECTORY SCANNING IN A PRODUCTION ENVIRONMENT
-    RocketSled::autoload(DataBank::autoload());
+var_dump(php_uname('n'));die();
 
-    //RUN THAT SHIT! http://www.youtube.com/watch?v=N_PX3DWH0nE
-    RocketSled::run();
+require_once('config.php');
+//OVERRIDE THE DEFAULT AUTOLOAD TO PREVENT REPEATED
+//DIRECTORY SCANNING IN A PRODUCTION ENVIRONMENT
+RocketSled::autoload(DataBank::autoload());
+
+//RUN THAT SHIT! http://www.youtube.com/watch?v=N_PX3DWH0nE
+RocketSled::run();
