04a317391a0b9a4b90c4323aec896b6299d9e3e6
Gegham Ayvazyan <gagvirus92@gmail.com>
2017-08-06 21:39:10 +0400

    Added message listing functionality

--- public/Actions/dashboard.class.php
+++ public/Actions/dashboard.class.php
@@ -10,6 +10,29 @@ class Dashboard implements Runnable
 
     public function run()
     {
-        echo view('dashboard', 'My Messages');
+        $view = view('dashboard', 'My Messages');
+        $data = \Plusql::from(DB_CONNECTION_NAME)
+            ->message
+            ->sent_message
+            ->receiver
+            ->select('*')
+            ->where('user_id = "' . $_SESSION['uid'] . '"')
+            ->run();
+        $node = $view->repeat('.message-row');
+        while ($message = $data->nextRow())
+        {
+//            var_dump($message);
+//            dd($message);
+            $node->setValue('.message-id', $message['message_id']);
+            $node->setValue('.message-text', $message['message_text']);
+            $node->setValue('.message-type', $message['message_type']);
+            $node->setValue('.message-receiver', $message['message_text']);
+            $node->next();
+        }
+//        foreach ($data as $item) {
+//            dd(213);
+//            echo $item->message_text;
+//        }
+        echo $view;
     }
 }
